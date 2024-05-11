3024b30cd9e9c0e6af48bf75089c21896efca936
firstatack <joaquinbolson80@gmail.com>
2024-05-11 14:18:16 -0300

    library secretjenkins

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715374602',resources: [
+    cacheName: 'chirpy-1715447731',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
