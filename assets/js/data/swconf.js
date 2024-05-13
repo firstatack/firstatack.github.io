d8efe3189635e9914c8682d8dcd4579c561158ff
firstatack <joaquinbolson80@gmail.com>
2024-05-13 10:22:04 -0300

    breakssh

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715447731',resources: [
+    cacheName: 'chirpy-1715606361',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
