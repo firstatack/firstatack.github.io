03809f5ef42f6569f58e658ee14f782b1901bb39
firstatack <joaquinbolson80@gmail.com>
2024-05-31 09:55:57 -0300

    vivifytech

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717075146',resources: [
+    cacheName: 'chirpy-1717159906',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
