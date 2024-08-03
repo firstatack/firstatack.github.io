593b2db7007ab0a05a01979c39cdc1fd361b2cc8
firstatack <joaquinbolson80@gmail.com>
2024-08-03 15:21:38 -0300

    showtime

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722691578',resources: [
+    cacheName: 'chirpy-1722709039',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
