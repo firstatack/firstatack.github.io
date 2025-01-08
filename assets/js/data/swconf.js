41cee5acac742a01f9e672f3981c9eb50667bcb3
firstatack <joaquinbolson80@gmail.com>
2025-01-08 14:54:27 -0300

    file

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722709039',resources: [
+    cacheName: 'chirpy-1736358709',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
