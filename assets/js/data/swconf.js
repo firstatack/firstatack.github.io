5341a28a9aaee2afed95f53a95359e4ac16360a4
firstatack <joaquinbolson80@gmail.com>
2025-08-13 17:50:52 -0300

    pingctf

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752243577',resources: [
+    cacheName: 'chirpy-1755117877',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
