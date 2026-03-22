582b3a554794d4cec1c3945aba5aa397dd4fa10a
firstatack <joaquinbolson80@gmail.com>
2026-03-22 12:51:08 -0300

    correct

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774191429',resources: [
+    cacheName: 'chirpy-1774194582',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
