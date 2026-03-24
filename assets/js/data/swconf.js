233caf9fd2dddba5395ccca06a4b6bdb5047be83
firstatack <joaquinbolson80@gmail.com>
2026-03-24 12:43:04 -0300

    bigwear-corrciones

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774366647',resources: [
+    cacheName: 'chirpy-1774366919',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
